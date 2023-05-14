74fcfb47ff064bfd08452fc62ca8e5ff3314fee6
lemurtronic <leigh@xanthic.co.uk>
2023-05-14 18:20:45 +0100

    updated loop

--- cam_loop.sh
+++ cam_loop.sh
@@ -12,25 +12,17 @@ do
 
   if [[ $(date +%s) < $(($(date -d "$Dawn" +%s) + 84600)) && $(date +%s) > $(date -d "$Dusk" +%s) ]]
   then
-    libcamera-still --shutter $shutter -e png -o images/snapshot.png --width 1024 --height 760 --immediate
-
-    # Copy the file to the images folder, for processing later
-    cp images/snapshot.png images/$(date +%Y%m%d%H%M%S).png
-
-    # Add the date to the image
-    convert images/snapshot.png -pointsize 12 -fill white -undercolor '#00000080' -gravity SouthWest -annotate +10+10 "$(date)" images/current.jpg
-
-    # Upload the image to the website
-sshpass -p $sftp_pass sftp u350931518@home349728106.1and1-data.host <<EOF
-put images/current.jpg
-bye
-EOF
+    libcamera-still --shutter $shutter --gain 1.5 -e png -o images/snapshot.png --immediate
+   #libcamera-still --shutter $shutter -e png -o images/snapshot.png --width 1024 --height 760 --immediate
 
     sleep $(( 30 - $shutter/1000000 ))
 
   else
-    libcamera-still -e png -o images/snapshot.png --width 800 --height 600 --immediate
+   #libcamera-still -e png -o images/snapshot.png --width 800 --height 600 --immediate
+    libcamera-still -e png -o images/snapshot.png --immediate
 
+    sleep 25 
+  fi
     # Copy the file to the images folder, for processing later
     cp images/snapshot.png images/$(date +%Y%m%d%H%M%S).png
 
@@ -42,7 +34,4 @@ sshpass -p $sftp_pass sftp u350931518@home349728106.1and1-data.host <<EOF
 put images/current.jpg
 bye
 EOF
-
-    sleep 25 
-  fi
 done
